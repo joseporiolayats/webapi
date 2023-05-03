78eca93062dccdfc5ff02cd55b5528a5617bf273
joseporiolayats <oriol.ayats@gmail.com>
2023-05-03 22:46:47 +0200

    Placeholder for first script

--- webapi/app.py
+++ webapi/app.py
@@ -3,5 +3,23 @@ webapi/app.py
 
 Just a placeholder starting script.
 """
+from fastapi import FastAPI
 
-print("Hello WebAPI")
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+# aqui es on el codi s'engega
+def app() -> None:
+    """
+    Placeholder for doing something
+    """
+    print("Loading app")
+
+
+if __name__ == "__main__":
+    print("Hello WebAPI")
