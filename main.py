77bf63c51f07df35e672834fa75c8e47ca892a16
joseporiolayats <oriol.ayats@gmail.com>
2023-05-05 00:23:50 +0200

    Added main routers and middleware access

--- main.py
+++ main.py
@@ -6,17 +6,39 @@ Main script for starting the app
 import uvicorn
 from decouple import config
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
+
+from webapi.routers.clients import router as clients_router
+from webapi.routers.policies import router as policies_router
+
+# from webapi.routers.users import router as users_router
+
 
 DB_URL = config("DB_URL", cast=str)
 DB_NAME = config("DB_NAME", cast=str)
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello FastAPI"}
+app.include_router(clients_router, prefix="/clients", tags=["clients"])
+app.include_router(policies_router, prefix="/policies", tags=["policies"])
+# app.include_router(users_router, prefix="/users", tags=["users"])
 
 
 @app.on_event("startup")
