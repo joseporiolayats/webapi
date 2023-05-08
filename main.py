5138f3cd094d957c9b38c24d61756cc69dd6514c
joseporiolayats <oriol.ayats@gmail.com>
2023-05-08 22:19:40 +0200

    Main file working

--- main.py
+++ main.py
@@ -3,22 +3,20 @@ main.py
 
 Main script for starting the app
 """
-import logging
-
 import uvicorn
 from decouple import config
+from fastapi import Depends
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 
+from webapi.backend.authentication import Authorization
 from webapi.data.dataflow import DataFlow
+from webapi.logs.logger import app_logger
 from webapi.routers.clients import router as clients_router
 from webapi.routers.policies import router as policies_router
 from webapi.routers.users import router as users_router
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 # Read environment variables
 DB_URL = config("DB_URL", cast=str)
@@ -27,6 +25,8 @@ DB_COLLECTION_CLIENTS = config("DB_COLLECTION_CLIENTS", cast=str)
 DB_COLLECTION_POLICIES = config("DB_COLLECTION_POLICIES", cast=str)
 DB_CLIENTS = config("DB_CLIENTS", cast=str)
 DB_POLICIES = config("DB_POLICIES", cast=str)
+SECRET_KEY = config("SECRET_KEY", cast=str)
+ACCESS_TOKEN_EXPIRE_SECONDS = config("ACCESS_TOKEN_EXPIRE_SECONDS", cast=int)
 
 # Define allowed origins for CORS
 origins = [
@@ -47,11 +47,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Authentication
+authorization = Authorization(secret=SECRET_KEY)
+
+
 # Include routers
-app.include_router(clients_router, prefix="/clients", tags=["clients"])
-app.include_router(policies_router, prefix="/policies", tags=["policies"])
-app.include_router(users_router, prefix="/users", tags=["users"])
-app.include_router(users_router, prefix="/gateway", tags=["gateway"])
+
+app.include_router(
+    clients_router,
+    prefix="/clients",
+    tags=["clients"],
+    dependencies=[Depends(authorization.auth_wrapper)],
+)
+app.include_router(
+    users_router,
+    prefix="/users",
+    tags=["users"],
+)
+app.include_router(
+    policies_router,
+    prefix="/policies",
+    tags=["policies"],
+    dependencies=[Depends(authorization.auth_wrapper)],
+)
 
 
 @app.on_event("startup")
@@ -69,11 +87,15 @@ async def startup_db_client():
         )
 
         collections_in_remote_db = await app.mongodb.list_collection_names()
+        app_logger.info(
+            f"The Database has {len(collections_in_remote_db)} "
+            f"collections: {collections_in_remote_db}"
+        )
 
-        if f"{DB_NAME}.{DB_COLLECTION_CLIENTS}" not in collections_in_remote_db:
+        if f"{DB_COLLECTION_CLIENTS}" not in collections_in_remote_db:
             await dataflow.fill_database(url=DB_CLIENTS, db_name=DB_COLLECTION_CLIENTS)
 
-        if f"{DB_NAME}.{DB_COLLECTION_POLICIES}" not in collections_in_remote_db:
+        if f"{DB_COLLECTION_POLICIES}" not in collections_in_remote_db:
             await dataflow.fill_database(
                 url=DB_POLICIES, db_name=DB_COLLECTION_POLICIES
             )
@@ -85,9 +107,9 @@ async def startup_db_client():
         app.cache["policies"] = await dataflow.load_cache(
             url=DB_POLICIES, db_name=DB_COLLECTION_POLICIES
         )
-        logger.info("Database and cache initialized successfully.")
+        app_logger.info("Database and cache initialized successfully.")
     except Exception as e:
-        logger.error(f"Error initializing database and cache: {e}")
+        app_logger.error(f"Error initializing database and cache: {e}")
 
 
 @app.on_event("shutdown")
@@ -96,10 +118,12 @@ async def shutdown_db_client():
     Close the database connection on shutdown.
     """
     try:
+        app.mongodb_client = AsyncIOMotorClient(DB_URL)
+        app.mongodb = app.mongodb_client[DB_NAME]
         app.mongodb_client.close()
-        logger.info("Database connection closed successfully.")
+        app_logger.info("Database connection closed successfully.")
     except Exception as e:
-        logger.error(f"Error closing database connection: {e}")
+        app_logger.error(f"Error closing database connection: {e}")
 
 
 if __name__ == "__main__":
