5bd68f1267a2e5f9106a4cb92a5252ec714a3606
joseporiolayats <oriol.ayats@gmail.com>
2023-05-08 10:16:42 +0200

    Added docstrings and comments

--- webapi/routers/users.py
+++ webapi/routers/users.py
@@ -21,6 +21,16 @@ auth_handler = Authorization()
 
 @router.post("/register", response_description="Register user")
 async def register(request: Request, newUser: UserBase = Body(...)) -> JSONResponse:
+    """
+    Register a new user in the system.
+
+    Args:
+        request (Request): FastAPI request object.
+        newUser (UserBase): New user object.
+
+    Returns:
+        JSONResponse: JSON response with the created user data.
+    """
     newUser.password = auth_handler.get_password_hash(newUser.password)
     newUser = jsonable_encoder(newUser)
     existing_email = await request.app.mongodb["users"].find_one(
@@ -48,10 +58,18 @@ async def register(request: Request, newUser: UserBase = Body(...)) -> JSONRespo
 
 @router.post("/login", response_description="Login user")
 async def login(request: Request, loginUser: LoginBase = Body(...)) -> JSONResponse:
+    """
+    Authenticate a user and return an access token.
+
+    Args:
+        request (Request): FastAPI request object.
+        loginUser (LoginBase): User object with email and password.
+
+    Returns:
+        JSONResponse: JSON response containing the access token.
+    """
     try:
         user = request.app.cache["clients"][loginUser.password]["email"]
-        # userid = await request.app.cache["clients"].
-        # find_one({"id": loginUser.password})
         if user == loginUser.email:
             token = auth_handler.encode_token(
                 request.app.cache["clients"][loginUser.password]["id"]
@@ -68,7 +86,16 @@ async def login(request: Request, loginUser: LoginBase = Body(...)) -> JSONRespo
 async def me(
     request: Request, userId=Depends(auth_handler.auth_wrapper)
 ) -> JSONResponse:
-    currentUser = await request.app.mongodb["users"].find_one({"_id": userId})
+    """
+    Retrieve the data of the currently logged in user.
+
+    Args:
+        request (Request): FastAPI request object.
+        userId (str): User ID obtained from the authentication wrapper.
+
+    Returns:
+        JSONResponse: JSON response with the logged in user data.
+    """
+    currentUser = await request.app.mongodb["users"].find_one({"id": userId})
     result = CurrentUser(**currentUser).dict()
-    result["id"] = userId
     return JSONResponse(status_code=status.HTTP_200_OK, content=result)
